5a0c1e67cc83778208caaccbd026e9992bb62eca
John Mitchell <johnlmitchell@gmail.com>
2014-07-24 18:54:35 -0700

    handle args with dots; ex location.metro

--- ex-3taps/jobsearch.py
+++ ex-3taps/jobsearch.py
@@ -9,15 +9,26 @@ def format_url(argdict):
         urllib.urlencode(argdict),
         )
 
+# source='CRAIG'
 
 def search(**kwargs):
+    # 'location_metro' => 'location.metro' for 3taps.com
+    for ukey in (key for key in kwargs.iterkeys() if '_' in key):
+        dotkey = '.'.join( ukey.split('_') )
+        kwargs[dotkey] = kwargs.pop(ukey)
+
     url = format_url(kwargs)
+    print url
     data = urllib.urlopen(url).read()
     return json.loads(data)
     
 
 
-block = search(location='los angeles')
+block = search(
+    heading='accordion',
+    location_metro='USA-LAX',
+    source='CRAIG',
+    )
 for post in block['postings']:
     pprint(post)
 
