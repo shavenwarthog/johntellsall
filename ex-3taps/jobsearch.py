1b95adcaecd9939a8af4dd846a52dfd40c199b87
John Mitchell <johnlmitchell@gmail.com>
2014-07-25 15:49:51 -0700

    print counts of each job type

--- ex-3taps/jobsearch.py
+++ ex-3taps/jobsearch.py
@@ -12,39 +12,59 @@ def format_url(argdict):
         urllib.urlencode(argdict),
         )
 
+def do_search(query):
+    query = query.copy()
+    # 'location_metro' => 'location.metro' for 3taps.com
+    for ukey in (key for key in query.iterkeys() if '_' in key):
+        dotkey = '.'.join( ukey.split('_') )
+        query[dotkey] = query.pop(ukey)
+
+    url = format_url(query)
+    return url, json.loads( urllib.urlopen(url).read() )
+
+
+class TTapsResult(object):
+    def __init__(self, **kwargs):
+        self.query = kwargs
+        self.url, self.block = do_search(self.query)
+
+
+    def __iter__(self):
+        return iter(self.block)
+
+
+    def __getitem__(self, key):
+        return self.block[key]
+
+
+    
+# XXXXX: doesnt work: category_group='JJJJ',
+# count='category_group', # X: count mode
+# source='CRAIG',
+# price='..500',
 # source='CRAIG'
 
-def search(**kwargs):
-    # 'location_metro' => 'location.metro' for 3taps.com
-    for ukey in (key for key in kwargs.iterkeys() if '_' in key):
-        dotkey = '.'.join( ukey.split('_') )
-        kwargs[dotkey] = kwargs.pop(ukey)
-
-    url = format_url(kwargs)
-    data = urllib.urlopen(url).read()
-    return json.loads(data)
-    
 
 for term in ['python','java','django','php']:
-    block = search(
-        # XXXXX: doesnt work: category_group='JJJJ',
+    result = TTapsResult(
         category='JWEB|JCON|JENG',
-        count='category_group', # X: count mode
         heading=term,
+        count='category_group', # X: count mode
         location_metro='USA-LAX',
-        # source='CRAIG',
-        # price='..500',
         retvals='external_url,heading,category,category_group,status,price',
     )
-    if 'postings' in block:
-        print '{:10} {}'.format(term, len(block['postings']))
-        for post in block['postings']:
+    if 'postings' in result:
+        print '{:10} {}'.format(term, len(result['postings']))
+        pprint(result.block); continue
+        for post in result['postings']:
             if 1:
                 pprint(post)
             else:
                 print post['heading']
     else:
-        print term,':'
-        pprint(block)
+        print '{:12} {:3d}'.format(
+            term,
+            result.block['num_matches'],
+            )
 
 
